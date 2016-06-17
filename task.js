4282443ef8018d1c72beb9aecb19e19ebe26400e
Mike Starr <mstarr@rsui.com>
2016-06-17 13:56:34 -0400

    added node demo

---
+++ task.js
@@ -0,0 +1,18 @@
+//noinspection JSLint
+'use strict';
+
+var Task = function (name) {
+    this.name = name;
+    this.completed = false;
+};
+
+Task.prototype.complete = function () {
+    console.log('completing task: ' + this.name);
+    this.completed = true;
+};
+
+Task.prototype.save = function () {
+    console.log('saving task: ' + this.name);
+};
+
+module.exports = Task;
